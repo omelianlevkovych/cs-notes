299b64ed57c368bf10888d55daa1ed515e7a29c1
Omelian Levkovych <omelianai@gmail.com>
2022-03-27 21:28:13 +0300

    added the consumer

--- RMQ/.net/01.hello-world/Receive/Receive.cs
+++ RMQ/.net/01.hello-world/Receive/Receive.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" }; 
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+// the consumer should also create queue, you can ask why so?
+// well because we might start the consumer before the publisher, we want
+// to make sure that the queue exists before we try to consume messages from it.
+channel.QueueDeclare("hello-world-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+channel.BasicConsume(queue: "hello-world-queue",
+        autoAck: true,
+        consumer: consumer);
+
+Console.ReadLine();
