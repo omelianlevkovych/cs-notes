8409513aff9ee74caa77f615453f0ad4fb4d9690
Omelian Levkovych <omelianai@gmail.com>
2022-03-27 21:17:42 +0300

    updated doc added publisher

--- RMQ/.net/01.hello-world/Send/Send.cs
+++ RMQ/.net/01.hello-world/Send/Send.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello-world-queue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "",
+    routingKey: "hello-world-queue",
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine(message);
+Console.ReadLine();
